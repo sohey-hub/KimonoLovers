e626b706a6914fa2b9f4bf968c5c7cfe15319008
sohey-hub <stcydgtsc7@gmail.com>
2021-01-25 23:57:29 +0900

    関東エリアの実装

---
+++ app/controllers/kantos_controller.rb
@@ -0,0 +1,18 @@
+class KantosController < ApplicationController
+
+
+  def index
+    @items = Item.where(region_id: 4)
+    if @items.present?
+      render :index
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :explanation, :region_id, :genre_id, :image).merge(user_id: current_user.id)
+  end
+end
