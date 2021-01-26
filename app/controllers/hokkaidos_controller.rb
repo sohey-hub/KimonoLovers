483b3817dd45effe4aa07291424730dc9fd8b7bd
sohey-hub <stcydgtsc7@gmail.com>
2021-01-26 13:13:02 +0900

    北海道エリアの実装

---
+++ app/controllers/hokkaidos_controller.rb
@@ -0,0 +1,16 @@
+class HokkaidosController < ApplicationController
+  def index
+    @items = Item.where(region_id: 2)
+    if @items.present?
+      render :index
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :explanation, :region_id, :genre_id, :image).merge(user_id: current_user.id)
+  end
+end
---
+++ app/helpers/hokkaidos_helper.rb
@@ -0,0 +1,2 @@
+module HokkaidosHelper
+end
